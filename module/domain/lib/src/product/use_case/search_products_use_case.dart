61e6a600b58cc0f78c98779f520d2d6b85f1ede6
David Santacoloma <adsi1409@hotmail.com>
2025-09-25 11:04:43 -0500

    Add use case to search products

---
+++ module/domain/lib/src/product/use_case/search_products_use_case.dart
@@ -0,0 +1,13 @@
+import 'package:domain/src/product/entity/product.dart';
+import 'package:domain/src/product/repository/product_repository.dart';
+
+class SearchProductsUseCase {
+  final ProductRepository _productRepository;
+  final int _page;
+
+  SearchProductsUseCase(this._productRepository, this._page);
+
+  Future<List<Product>> call(String query, int page) {
+    return _productRepository.searchProducts(query: query, page: _page);
+  }
+}
