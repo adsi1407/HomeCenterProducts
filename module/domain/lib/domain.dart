caa4abd5f8b3a20906712185919614a6ad40bd39
David Santacoloma <adsi1409@hotmail.com>
2025-09-29 07:51:01 -0500

    Arrange export entries in barrel files

--- module/domain/lib/domain.dart
+++ module/domain/lib/domain.dart
@@ -1,14 +1,21 @@
 library;
 import 'package:injectable/injectable.dart';
 
+// Domain public API barrel
+// Group exports by concept for easier maintenance.
+
+// Entities
 export 'src/product/entity/product.dart';
-export 'src/product/repository/product_repository.dart';
-export 'src/product/use_case/search_products_use_case.dart';
 export 'src/cart_item/entity/cart_item.dart';
+
+// Repositories (interfaces)
+export 'src/product/repository/product_repository.dart';
 export 'src/cart_item/repository/cart_item_repository.dart';
-export 'src/cart_item/use_case/cart_item_use_case.dart';
-// suggestion exports
 export 'src/suggestion/repository/suggestion_repository.dart';
+
+// Use cases
+export 'src/product/use_case/search_products_use_case.dart';
+export 'src/cart_item/use_case/cart_item_use_case.dart';
 export 'src/suggestion/use_case/get_suggestions_use_case.dart';
 
 @InjectableInit.microPackage()
--- module/infrastructure/lib/infrastructure.dart
+++ module/infrastructure/lib/infrastructure.dart
@@ -2,7 +2,7 @@ library;
 
 /// Public exports for the infrastructure package.
 export 'src/cart/app_database.dart';
-export 'src/product/product_api.dart';
+// product_api is an internal implementation detail; export only what's public.
 
 import 'package:injectable/injectable.dart';
 
